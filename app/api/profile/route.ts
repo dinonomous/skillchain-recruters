15bb9bcd4fc3dede040b46a6b86335ed9b8cff94
Dineshwar <133356394+dinonomous@users.noreply.github.com>
2025-01-29 15:33:45 +0530

    new3

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import fetch from "node-fetch";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { pipeline } from '@xenova/transformers';
 
 const apiKey: string = "sk_e2028d62c58a7bb305d09d9b108d803b50f227dd";
 const googleApiKey: string = "AIzaSyDW_N1sUzq3wwmHE3z3qo3Y9N1a6sIAyRM";
@@ -9,6 +10,8 @@ const genAi = new GoogleGenerativeAI(googleApiKey);
 interface ProfileResponse {
   [key: string]: any; // Update this based on actual API response
 }
+
+let pipe = await pipeline('sentiment-analysis');
 
 export async function GET() {
   try {
@@ -20,15 +23,13 @@ export async function GET() {
     }
 
     const profile: ProfileResponse = await response.json();
-
-    // Generate Content using Gemini AI
     const model = genAi.getGenerativeModel({ model: "gemini-pro" });
     const result = await model.generateContent(
       `Summarize this LinkedIn profile in a paragraph, mention achievements and include thenumber of years of each experience: ${JSON.stringify(profile)}`
     );
     const text = await result.response.text();
-
-    return NextResponse.json({ profile, summary: text }, { status: 200 });
+    let out = await pipe(text)
+    return NextResponse.json({ vector: out }, { status: 200 });
   } catch (error) {
     console.error("Error:", error);
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
